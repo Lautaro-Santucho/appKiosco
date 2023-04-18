f2adedd878c58e32f104ff0d131c72e7111ad8d9
Lautaro <santucholautaro437@gmail.com>
2023-04-17 23:11:05 -0300

    creacion botones CRUD moduloDeGrupo

--- moduloDeGrupo.py
+++ moduloDeGrupo.py
@@ -9,6 +9,7 @@ class moduloGrupo:
         self.vent.title("Modulo de grupo")
 
         self.__call__()
+        self.botonesCRUD()
 
         self.vent.mainloop()
 
@@ -28,5 +29,20 @@ class moduloGrupo:
         # Scrollbar
         self.scrollbar = ttk.Scrollbar(self.treeview, orient="vertical", command=self.treeview.yview())
         self.treeview["yscrollcommand"] = self.scrollbar.set
+
+#---------------------Inicio botones CRUD---------------------  
+
+    def botonesCRUD(self):
+        self.labelframe1= ttk.Labelframe(self.vent, text="Opciones")
+        self.labelframe1.place(x=510, y=0, height=315, width=180)
+
+        #Button
+        self.button1 = ttk.Button(self.labelframe1, text="Agregar", width=20).place(x=10, y=0, height=50, width=157)
+        self.button2 = ttk.Button(self.labelframe1, text="Modificar", width=20).place(x=10, y=60, height=50, width=157)
+        self.button3 = ttk.Button(self.labelframe1, text="Eliminar", width=20).place(x=10, y=120, height=50, width=157)
+        self.button4 = ttk.Button(self.labelframe1, text="Cancelar", width=20).place(x=10, y=180, height=50, width=157)
+        self.button5 = ttk.Button(self.labelframe1, text="Grabar", width=20).place(x=10, y=240, height=50, width=157)
+
+#---------------------Fin botones CRUD---------------------
 
     
